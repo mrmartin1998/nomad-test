bd64c8897eeae178baf614b58004fe3388fc6c46
Martin Emil Brabenec <martinemilbrabenec@gmail.com>
2025-05-28 09:36:57 +0200

    fix: update Jest config to be compatible with Next.js

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,14 @@
-module.exports = {
-  testEnvironment: 'jsdom',
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+})
+
+// Add any custom config to be passed to Jest
+const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   moduleNameMapper: {
     // Handle module aliases (if you're using them in your Next.js config)
@@ -9,4 +17,7 @@ module.exports = {
   transform: {
     '^.+\\.(js|jsx|ts|tsx|mjs)$': ['babel-jest', { presets: ['next/babel'] }],
   },
-}; 
+}
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig) 
